06522a2969a439467e86a674bf07b169ee28fb61
Shivu-a <sebaingame@gmail.com>
2022-08-19 23:43:52 -0300

    try

--- src/helpers/getMangaById.js
+++ src/helpers/getMangaById.js
@@ -1,7 +1,10 @@
 export const getMangaById = async (id) => {
   const url = `https://api.mangadex.org/manga/${id}`;
 
-  const respuesta = await fetch(url);
+  const respuesta = await fetch(url, {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  });
 
   const data = await respuesta.json();
 
