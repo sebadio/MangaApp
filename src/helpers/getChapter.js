a64fc6335c4108a2cc5bc4eb17a9fb610fb75c14
Shivu-a <sebaingame@gmail.com>
2022-08-19 23:40:28 -0300

    try

--- src/helpers/getChapter.js
+++ src/helpers/getChapter.js
@@ -1,5 +1,8 @@
 export const getChapter = async (id) => {
-  const respuesta = await fetch(`https://api.mangadex.org/chapter/${id}`);
+  const respuesta = await fetch(`https://api.mangadex.org/chapter/${id}`, {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  });
 
   const { data } = await respuesta.json();
 
